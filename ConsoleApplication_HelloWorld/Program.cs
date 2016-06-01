cdcf6344c3b7aed508be830ce93f22b61dc4e392
kontrik <alex.nezhinskiy@gmail.com>
2016-06-01 09:26:41 +0300

    test

--- ConsoleApplication_HelloWorld/Program.cs
+++ ConsoleApplication_HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using myDataType = System.Int32;
 
 namespace ConsoleApplication_HelloWorld
 {
@@ -10,8 +11,34 @@ namespace ConsoleApplication_HelloWorld
     {
         static void Main()
         {
-            System.Console.WriteLine("Hello, World!");
-            System.Console.ReadKey();
+            bool a;
+            int X=2;
+                if ((X > 1) && (!(X < 5) && (X < 3)))
+                    Console.WriteLine("1. true");
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                if ((X % X > 1) || (X / X < X % X + 1))
+                    Console.WriteLine("2. true");
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                if ((X <= 2) || (X % 2 + 1 != 2) && (X == 2 && X < 4))
+                Console.WriteLine("3. true");
+                    else
+                    {
+                        Console.WriteLine("false");
+                    }
+                if ((X * X != 2 / X + 2) && (X < 1))
+                Console.WriteLine("4. true");
+                    else
+                    {
+                        Console.WriteLine("false");
+                    }
+
+            Console.ReadKey();
         }
     }
 }
